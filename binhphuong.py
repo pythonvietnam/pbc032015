b8e5ad613b5155ca2ec520025416970d45f3db39
Terry Pham <danghao.cntt@gmail.com>
2015-03-27 09:26:36 +0700

    'Xoa'

--- binhphuong.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-so=input("Nhap vao so can tinh binh phuong: ")
-bp=input("Nhap vao gia tri can so sanh: ")
-if so*so==bp:
-	print'Bang nhau roi'
-elif so*so<bp:
-	print 'Lon hon roi'
-elif so*so>bp:
-	print 'Nho hon roi'
--- hcn.py
+++
@@ -1,6 +0,0 @@
-import math
-print 'Chuong trinh tinh dien tich hinh chu nhat'
-dai=int(input('Vui long nhap chieu dai:'))
-rong=int(input('vui long nhap chieu rong:'))
-dientich=dai*rong 
-print dientich
