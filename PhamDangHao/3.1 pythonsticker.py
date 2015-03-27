ccae404523f2aca608771abe3c04b7c9a8ea9757
Terry Pham <danghao.cntt@gmail.com>
2015-03-27 18:31:47 +0700

    'Pythonsticker'

---
+++ PhamDangHao/3.1 pythonsticker.py
@@ -0,0 +1,23 @@
+while True:
+	a=raw_input('Nhap cac chuoi de hien thi sticker - Nhan Q de thoat')
+	if a =='q':
+		print 'Ban vua nhap phim Q! Chuong trinh se thoat'
+		break
+	else:
+		try:
+			if a=='1':
+				print (":)")
+			elif a=='2':
+				print (":))")
+			elif a=='3':
+				print ("=))")
+			elif a=='4':
+				print ("<3")
+			else:
+				print 'Khong co chuoi hien thi'
+			
+		except:
+			print 'Khong co ky tu nao duoc hien thi'
+		
+
+	
