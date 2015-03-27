50724c0e4861b345bf381f0da695db2bed764b29
skytb <vn.thang.nguyendinh@gmail.com>
2015-03-28 01:58:22 +0700

    sua code

--- NguyenDinhThang/binhphuong.py
+++ NguyenDinhThang/binhphuong.py
@@ -1,29 +1,33 @@
 import math
-while True:
-    try:
-        d =  str(raw_input("Ban co muon chay khong Y/N:"))
-        break
-        print "gia tri ",d
-    except ValueError:
-        print " oh oh he he"
-while str(d) == y:
-    print " Chao mung ban den chuong trinh"
+try:
+    # d =  str(raw_input("Ban co muon chay khong Y/N:"))    
+    # y = True
+    # while str(d) = y:
+    #     print " Chao mung ban den chuong trinh"
     while True:
         try:
-            a = input("Nhap so thu 1:")
+            a = int(raw_input("Nhap so thu 1:"))
             break
         except ValueError:
-            print " Ban phai nhap so"
-    b = input (" Nhap so thu 2:")
+            print " !----- Du lieu nhap dang so -----!"
+        
+    while True:
+        try:
+            b = int(raw_input("Nhap so thu 2:"))
+            break
+        except ValueError:
+            print " !----- Du lieu nhap dang so -----!"
     c = a*a
-    print "Binh phuong so 1 ",c
+    print "Binh phuong thu 1 ",c
     if c == b:
-        print " So ban nhap bang nhau"
-    else:
-        if c > b:
-                print " So ban nhap qua cao"
-        else:
-            if c < b:
+            print " So ban nhap bang nhau"
+    elif c > b:
+            print " So ban nhap qua cao"
+    elif c < b:
                     print " So ban nhap qua thap"
-else:        
-    print "Tam biet"
+    # else:        
+    #     print "Tam biet"
+except ValueError:
+    print " Loi khong xac dinh"
+finally:
+    print "Done!"
