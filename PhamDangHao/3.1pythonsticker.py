a92bf3fc0e35cddf61b42fb567c50dafd543b0f2
Terry Pham <danghao.cntt@gmail.com>
2015-03-27 19:05:41 +0700

    'Nopbaipythonsticker'

--- PhamDangHao/3.1pythonsticker.py
+++ PhamDangHao/3.1pythonsticker.py
@@ -1,23 +1,26 @@
 while True:
-	a=raw_input('Nhap cac chuoi de hien thi sticker - Nhan Q de thoat')
-	if a =='q':
-		print 'Ban vua nhap phim Q! Chuong trinh se thoat'
-		break
-	else:
-		try:
+	a=raw_input('Vui Long nhap ky tu de hien thi sticker! Nhap Q de thoat')
+	try: 
+		if a=='q':
+			print 'Ban vua nhap ky tu Q, Chuong trinh se tu dong thoat'
+			break
+		else:
 			if a=='1':
-				print (":)")
+				print ':)'
 			elif a=='2':
-				print (":))")
+				print ':))'
 			elif a=='3':
-				print ("=))")
+				print '=)'
 			elif a=='4':
-				print ("<3")
+				print '=))'
+			elif a=='5':
+				print '<3'
 			else:
-				print 'Khong co chuoi hien thi'
-			
-		except:
-			print 'Khong co ky tu nao duoc hien thi'
+				'Ban vua nhap 1 ky tu khong phu hop! Vui long lam lai' 
+	except:
+		pass
+
 		
 
-	
+		
+		
--- hanguyengenet-baitap/3.1So_binh _phuong.py
+++
@@ -1,9 +0,0 @@
-import math
-a = int(raw_input('Nhap a: '))
-b = int(raw_input('Nhap binh phuong cua a: '))
-if b == a*a:
-    print 'Chinh xac!'
-elif b>a*a:
-    print 'So binh phuong ban nhap vao qua cao!'
-else:
-    print 'So binh phuong ban nhap vao qua thap!'
