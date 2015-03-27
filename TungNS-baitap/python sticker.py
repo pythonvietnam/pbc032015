e1e795de532e252c573c2c6b6382b4a77583e762
hellobeo <tungns.yb@gmail.com>
2015-03-27 18:29:28 +0700

    them thu muc bai tap cua Ng Son Tung

---
+++ TungNS-baitap/python sticker.py
@@ -0,0 +1,39 @@
+#python sticker
+
+print '\n ---Xin chao,test voi chuong trinh python sticker---'	
+print "\n nhap 1 de hien thi :) "
+print " nhap 2 de hien thi :( "
+print ' nhap 3 de hien thi (~.~) '
+print ' nhap 4 de hien thi =)) '
+print ' nhap 5 de hien thi 8-X '
+print ' nhap 6 de hien thi o.o!'
+print ' nhap 7 de hien thi 0};- '
+print ' nhap 8 de hien thi =.=! '
+running = True
+while running :
+	n = input("\n ban thich bieu tuong nao :")
+	while n != 'q':
+		if n == 1 :
+			print ' \n cuoi len nao :) '
+			break	
+		elif n == 2 :
+			print ' \n so sad :( '
+			break
+		elif n == 3 :
+			print ("\n (~.~) ")
+			break				
+		elif n == 4 :
+			print ("\n =)) ")
+			break			
+		elif n == 5 :
+			print ("\n 8-X ")
+			break	
+		elif n == 6 :
+			print ("\n o.o! ")
+			break		
+		elif n == 7 :
+			print ("\n 0};- ")
+			break				
+		elif n == 8 :
+			print ("\n =.=!")
+			break
