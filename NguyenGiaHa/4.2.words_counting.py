4955a73d59904d128197078aa5f1976c9182aac8
hanguyengenet <hanguyen.genet@gmail.com>
2015-03-30 10:05:37 +0700

    Update 4.2.words_counting.py

--- NguyenGiaHa/4.2.words_counting.py
+++ NguyenGiaHa/4.2.words_counting.py
@@ -18,9 +18,9 @@ while running:
 				d[l[i]]=d[l[i]]+1
 			else:
 				d[l[i]]=1
-		print d
+		print 'Doan van ban vua nhap co '+str(len(l))+' tu.'
 		for k in d:
-			print "Tu '"+str(k)+"' xuat hien "+str(d[k])+ ' lan.'
+			print "'"+str(k)+"' xuat hien "+str(d[k])+ ' lan.'
 	elif answer=='2':
 		#char counting
 		s = raw_input('Hay nhap 1 doan van ban: ')
@@ -36,7 +36,7 @@ while running:
 		print 'Doan van ban vua nhap co '+str(j)+' ki tu.'
 		print d
 		for c in d:
-			print 'Ki tu '+str(c)+' da xuat hien '+str(d[c]) +' lan.'
+			print '''+str(c)+' xuat hien '+str(d[c]) +' lan.'
 	elif answer=='3':
 		running=False
 		print 'Quit!'
